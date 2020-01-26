a9e2f2976bbbc2bb008fd122fed4af1b1ef48012
gavinking <gavin@ceylon-lang.org>
2020-01-26 13:40:29 +0100

    Got unary +, - working again
    (They were unimplemented on the new 6 branch)

--- hibernate-core/src/main/java/org/hibernate/query/sqm/sql/BaseSqmToSqlAstConverter.java
+++ hibernate-core/src/main/java/org/hibernate/query/sqm/sql/BaseSqmToSqlAstConverter.java
@@ -1427,9 +1427,8 @@ public abstract class BaseSqmToSqlAstConverter
 		try {
 			return new UnaryOperation(
 					interpret( expression.getOperation() ),
-					(Expression) expression.getOperand().accept( this ),
-					determineValueMapping( expression )
-
+					toSqlExpression( expression.getOperand().accept(this) ),
+					(BasicValuedMapping) determineValueMapping( expression.getOperand() )
 			);
 		}
 		finally {
@@ -1715,7 +1714,7 @@ public abstract class BaseSqmToSqlAstConverter
 			magnitude = new UnaryOperation(
 					UNARY_MINUS,
 					magnitude,
-					magnitude.getExpressionType()
+					(BasicValuedMapping) magnitude.getExpressionType()
 			);
 		}
 
